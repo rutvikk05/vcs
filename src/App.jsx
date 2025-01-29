ec76bfd701921d8f7627081fdf841e1bb3a84ea9
rutvikk05 <rutvikk5@gmail.com>
2025-01-29 19:09:36 +0530

    updated navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navigation from './Customer/Components/Navigation'
+function App() {
+ 
+
+  return (
+    <>
+   <div>
+    <Navigation></Navigation>
+   </div>
+    </>
+  )
+}
+
+export default App
